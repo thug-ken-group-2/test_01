47c637c1840bc741c184d664044f3f9a8143502f
222hojun <sciboviator@gmail.com>
2024-02-23 17:04:40 +0900

    frontend commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,8 @@ public class Test {
     public void test(){
         System.out.println("This is a test.");
     }
+
+    public void test2() {
+        System.out.println("test again ..... from frontend branch");
+    }
 }

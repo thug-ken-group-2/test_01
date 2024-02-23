9190eccc08c0905e142953bd96e21678563069dd
chamele-on <skykitty13@gmail.com>
2024-02-23 17:29:23 +0900

    message

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,12 +7,6 @@ public class Test {
     public void test3() {
         System.out.println("abcdefg");
     }
-    public void test2() {
-        System.out.println("test again ..... from frontend branch");
 
-    }
 
-    public void cry(){
-        System.out.println("cry");
-    }
 }
